0fefc65622007c4466fb2ca65a2ace5b9ed0da41
Chinmay014 <chinmays117@gmail.com>
2023-10-02 18:17:54 +0200

    add code to show data on GUI

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import typing
 from PyQt6 import QtCore
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QWidget, QLabel, QGridLayout, QLineEdit, QMainWindow,QPushButton,QTableWidget
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QWidget, QLabel, QGridLayout, QLineEdit, QMainWindow,QPushButton,QTableWidget, QTableWidgetItem
 from PyQt6.QtGui import QAction
-import sys
+import sys,sqlite3
 # from datetime import datetime
 
 class MainWindow(QMainWindow):
@@ -25,13 +25,23 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget() 
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("id","Name","Course","Mobile"))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
     
-
+    def load_data(self):
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number,row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number,column_number,QTableWidgetItem(str(data)))
+        connection.close()
 
 app = QApplication(sys.argv)
 main_window = MainWindow()
 main_window.show()
+main_window.load_data()
 sys.exit(app.exec())
 
 
